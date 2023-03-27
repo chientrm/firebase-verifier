1d0f49d44a16c0a0a51f56421ab483cd74027351
Chien Tran <chientran@Chiens-MacBook-Pro.local>
2023-03-28 05:55:02 +0700

    remove project_id

--- index.ts
+++ index.ts
@@ -15,66 +15,48 @@ const forIdTokenPublicKeys = fetch(
   appCheckJWKS = createRemoteJWKSet(
     new URL("https://firebaseappcheck.googleapis.com/v1beta/jwks")
   ),
-  verifyIdToken = ({
-    idToken,
-    project_id,
-  }: {
-    idToken: string;
-    project_id: string;
-  }) =>
+  verifyIdToken = (idToken: string, project_no: string) =>
     Promise.all([decodeProtectedHeader(idToken), forIdTokenPublicKeys])
       .then(([header, publicKeys]) => publicKeys[header.kid!])
       .then((certificate) => importX509(certificate, "RS256"))
       .then((key) =>
         jwtVerify(idToken, key, {
-          issuer: `https://securetoken.google.com/${project_id}`,
-          audience: project_id,
+          issuer: `https://securetoken.google.com/${project_no}`,
+          audience: project_no,
         })
       )
       .then((result) => result.payload),
-  verifyAppCheckToken = ({
-    appCheckToken,
-    project_id,
-    project_no,
-  }: {
-    appCheckToken: string;
-    project_id: string;
-    project_no: string;
-  }) =>
+  verifyAppCheckToken = (appCheckToken: string, project_no: string) =>
     jwtVerify(appCheckToken, appCheckJWKS, {
       issuer: `https://firebaseappcheck.googleapis.com/${project_no}`,
-      audience: `projects/${project_id}`,
+      audience: `projects/${project_no}`,
     }).then((res) => res.payload),
   authVerifier =
-    (project_id: string) =>
+    (project_no: string) =>
     async (req: Request, res: Response, next: NextFunction) => {
       const authorization = req.header("Authorization")!,
         idToken = authorization.split(" ")[1],
-        user = await verifyIdToken({ idToken, project_id });
+        user = await verifyIdToken(idToken, project_no);
       res.locals.user = user;
       next();
     },
   appCheckVerifier =
-    ({ project_no, project_id }: { project_id: string; project_no: string }) =>
+    (project_no: string) =>
     async (req: Request, res: Response, next: NextFunction) => {
       const appCheckToken = req.header("X-Firebase-AppCheck")!,
-        device = await verifyAppCheckToken({
-          appCheckToken,
-          project_no,
-          project_id,
-        });
+        device = await verifyAppCheckToken(appCheckToken, project_no);
       res.locals.device = device;
       next();
     },
   firebaseVerifier =
-    ({ project_no, project_id }: { project_id: string; project_no: string }) =>
+    (project_no: string) =>
     async (req: Request, res: Response, next: NextFunction) => {
       const authorization = req.header("Authorization")!,
         idToken = authorization.split(" ")[1],
         appCheckToken = req.header("X-Firebase-AppCheck")!,
         [user, device] = await Promise.all([
-          verifyIdToken({ idToken, project_id }),
-          verifyAppCheckToken({ appCheckToken, project_no, project_id }),
+          verifyIdToken(idToken, project_no),
+          verifyAppCheckToken(appCheckToken, project_no),
         ]);
       res.locals.user = user;
       res.locals.device = device;
